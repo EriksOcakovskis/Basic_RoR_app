80676c744cf302aef865b147209e300ec6f93de1
Eriks Ocakovskis <e.ocakovskis@gmail.com>
2013-05-26 19:10:14 +0300

    Initial commit. Starting with Ruby 1.9.3 and Rails 3.2.13

---
+++ README.md
@@ -0,0 +1,3 @@
+A basic Rails app.
+
+This is a basic application that shows how Ruby on Rails handles mostly static web pages.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BasicApp::Application.config.secret_token = '24778b0478c02c0ae51eaf3eb46ea559482e4af5315ce45a5a8877ca2f14bb21d95f2409274736736843de57ba72f77befabb22a15ca74afcfccd6493bbbefff'
